1c41d1c873677b493fe5a7678bfa794ad95a5cd3
James Feng Cao <jamesfengcao@torapp.info>
2024-11-14 19:23:09 +0800

    simplify

--- index.mjs
+++ index.mjs
@@ -1,10 +1,12 @@
 export const handler = async (event) => {
-  const url = event.requestContext.path.substring(1);
+  const path = event.rawPath;
+  const url = path.substring(1);
   const Url = new URL(url);
   const newReq = new Request(Url, {
-    method: event.httpMethod,
+    method: event.requestContext.http.method,
     headers: event.headers,
     body: event.body,
-    });
+    redirect: 'follow'
+  });
   return await fetch(newReq);
 };
